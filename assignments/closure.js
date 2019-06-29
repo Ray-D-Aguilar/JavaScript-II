f30573db94d2dfadd2cd8925c6de13c190724755
Ray Aguilar <ray.aguilar15@gmail.com>
2019-06-29 09:27:29 -0500

    Added a console log for the firstName function

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 const name = "Ray";
-function firstName {
+function firstName () {
   return name;
 }
 
-firstName();
+console.log(firstName());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
